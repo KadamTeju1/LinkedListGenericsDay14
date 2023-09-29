0a804dfc3210d7dc849dd5ba1ace5b57bfdb3f93
KadamTeju <tejushinde8897@gmail.com>
2023-09-29 20:55:13 +0530

    UC4 Complete.

--- LinkedListGenericsDay14/src/Main.java
+++ LinkedListGenericsDay14/src/Main.java
@@ -1,15 +1,18 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Wellcome Data Structure Problems using Java Generics Project.");
 
-        LinkedListGenericsDay14<Integer> lgList = new LinkedListGenericsDay14<Integer>();
+        public static void main(String[] args) {
+            System.out.println("Wellcome Data Structure Problems using Java Generics Project.");
 
-        lgList.pushValue(70);
-        lgList.pushValue(30);
-        lgList.pushValue(56);
+            LinkedListGenericsDay14<Integer> lgList = new LinkedListGenericsDay14<Integer>();
 
-        lgList.displayList();
-    }
+            lgList.pushValue(70);
+            lgList.pushValue(56);
+
+            lgList.displayList();
+
+            lgList.insertValue(30,56);
+            lgList.displayList();
+        }
 }
